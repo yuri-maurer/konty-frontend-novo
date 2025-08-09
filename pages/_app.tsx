3a4c4e7c27fbd45ebb29e1e61c661a3a3dbd408d
Yuri Maurer <yuri@konty.com.br>
2025-08-09 16:08:00 -0300

    fix: corrigido atributo integrity no link do Font Awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps }: MyAppProps) {
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-          xintegrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
