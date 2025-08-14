8c4fe9dc706f8bdaf93507c6d13f2e851ea5ebf2
Yuri Maurer <yuri@konty.com.br>
2025-08-14 09:31:42 -0300

    fix(build): corrige typo 'integrity' no _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
-import { FavoritesProvider } from '../components/contexts/FavoritesContext'; // 1. Importe o provider
+import { FavoritesProvider } from '../components/contexts/FavoritesContext';
 
 interface MyAppProps extends AppProps<{ initialSession: Session }> {}
 
@@ -17,13 +17,13 @@ function MyApp({ Component, pageProps }: MyAppProps) {
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-      {/* 2. Envolve a aplicação com o FavoritesProvider */}
       <FavoritesProvider>
         <Head>
           <title>Konty Sistemas</title>
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+            // CORREÇÃO: A propriedade 'xintegrity' foi corrigida para 'integrity'.
             xintegrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
             crossOrigin="anonymous"
             referrerPolicy="no-referrer"
