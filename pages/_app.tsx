e86c17996cec677673053d3d0fbd0e7c1899ff33
Yuri Maurer <yuri@konty.com.br>
2025-08-14 09:56:46 -0300

    fix(build): remove atributos do link para forçar o build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,8 +23,6 @@ function MyApp({ Component, pageProps }: MyAppProps) {
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-            xintegrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-            crossOrigin="anonymous"
             referrerPolicy="no-referrer"
           />
         </Head>
